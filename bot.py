b079bfb15a82e21a4e33eb17cb4556927b0e67fe
bastosydaniel <bastosydaniel@gmail.com>
2022-08-12 19:22:48 -0300

    some changes on var names

--- bot.py
+++ bot.py
@@ -4,27 +4,27 @@ from selenium import webdriver
 import time
 
 
-navegador = webdriver.Chrome(executable_path=r'./chromedriver')
-navegador.get("https://notacarioca.rio.gov.br/senhaweb/login.aspx?ReturnUrl=%2Fcontribuinte%2Fnota.aspx")
+driver = webdriver.Chrome(executable_path=r'./chromedriver')
+driver.get("https://notacarioca.rio.gov.br/senhaweb/login.aspx?ReturnUrl=%2Fcontribuinte%2Fnota.aspx")
 time.sleep(1)
-navegador.find_element("xpath",'//*[@id="ctl00_cphCabMenu_tbCpfCnpj"]').send_keys("")
-navegador.find_element("xpath",'//*[@id="ctl00_cphCabMenu_tbSenha"]').send_keys("")
+driver.find_element("xpath",'//*[@id="ctl00_cphCabMenu_tbCpfCnpj"]').send_keys("")
+driver.find_element("xpath",'//*[@id="ctl00_cphCabMenu_tbSenha"]').send_keys("")
 # THIS TIME YOU USE TO WRITE THE CAPTCHA / USE ESSE TEMPO PARA PREENCHER O CAPTCHA
 time.sleep(15)
-navegador.find_element("xpath",'//*[@id="ctl00_cphCabMenu_btEntrar"]').click()
+driver.find_element("xpath",'//*[@id="ctl00_cphCabMenu_btEntrar"]').click()
 time.sleep(1)
 
-# navegador.find_element("xpath",'/html/body/form/div[3]/div[1]/div[6]/div/div/div/div/p/a[1]').click()
+# driver.find_element("xpath",'/html/body/form/div[3]/div[1]/div[6]/div/div/div/div/p/a[1]').click()
 
-cnpjtomador = 00
-tomador = navegador.find_element("xpath",'//*[@id="ctl00_cphCabMenu_tbCPFCNPJTomador"]')
+cnpj = 00
+tomador = driver.find_element("xpath",'//*[@id="ctl00_cphCabMenu_tbCPFCNPJTomador"]')
 tomador.get_attribute("type") == "number"
-tomador.send_keys(cnpjtomador)
+tomador.send_keys(cnpj)
 
-navegador.find_element("xpath",'//*[@id="ctl00_cphCabMenu_btAvancar"]').click()
+driver.find_element("xpath",'//*[@id="ctl00_cphCabMenu_btAvancar"]').click()
 
-navegador.find_element("xpath", '//*[@id="ctl00_cphCabMenu_tbDiscriminacao"]').send_keys("")
-navegador.find_element("xpath", '//*[@id="ctl00_cphCabMenu_tbValor"]').send_keys("")
-navegador.find_element("xpath",'//*[@id="ctl00_cphCabMenu_btEmitir"]').click()
+driver.find_element("xpath", '//*[@id="ctl00_cphCabMenu_tbDiscriminacao"]').send_keys("")
+driver.find_element("xpath", '//*[@id="ctl00_cphCabMenu_tbValor"]').send_keys("")
+driver.find_element("xpath",'//*[@id="ctl00_cphCabMenu_btEmitir"]').click()
 
 
